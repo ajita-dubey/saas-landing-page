494eb0e6f6f9821164f6d07c3286656bc99ea645
ajita-dubey <dubeyajita305@gmail.com>
2025-07-08 23:58:47 +0530

    header completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+// import './App.css'
+import Header from './section/Header'
+import Hero from './section/Hero'
+
+function App() {
+ 
+
+  return (
+    <main className=' overflow-hidden'>
+      <Header/>
+      <Hero/>
+    
+    </main>
+  )
+}
+
+export default App
