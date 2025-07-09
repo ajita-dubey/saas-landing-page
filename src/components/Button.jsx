b3146c5c39258c72a749d77eb2dec4bb7daf90f0
ajita-dubey <dubeyajita305@gmail.com>
2025-07-09 12:54:47 +0530

    button final

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,8 @@ import Marker from "./Marker.jsx";
 const Button = ({ icon, children, href,containerClassName,onClick,}) => {
   const Inner = () => (
     <>
-      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 
+      overflow-hidden">
         <span className="absolute -left-[1px]">
           <Marker  />
         </span>
